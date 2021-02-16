8ca517fb6e6bc374c63a45f449802412d7cd2617
heltonborges <heltonborgessilva@gmail.com>
2021-02-16 17:05:52 +0100

    Firebase

--- app/src/main/java/com/example/appbarproyect/MainActivity.kt
+++ app/src/main/java/com/example/appbarproyect/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.appbarproyect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de firebase")
+        analytics.logEvent("InitScreen", bundle)
     }
 }
